2167895e082a8082a61940b18ff9783e00d2ee5d
railken <railken@protonmail.ch>
2019-01-03 19:14:43 +0100

    add createAggregate and removeAggregate

--- src/Managers/AggregatorManager.php
+++ src/Managers/AggregatorManager.php
@@ -4,6 +4,10 @@ namespace Railken\Amethyst\Managers;
 
 use Railken\Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
+use Railken\Lem\Result;
+use Illuminate\Support\Collection;
+use Railken\Bag;
+use Closure;
 
 class AggregatorManager extends Manager
 {
@@ -13,4 +17,121 @@ class AggregatorManager extends Manager
      * @var string
      */
     protected $config = 'amethyst.aggregator.data.aggregator';
+
+
+    /**
+     * Aggregate multiple entities to one final entity
+     *
+     * @param array $sources
+     * @param array $weight
+     * @param Closure $onCreate
+     *
+     * @return Result
+     */
+    public function createAggregate(array $sources, array $weights = null, Closure $onCreate)
+    {
+    	if ($weights === null) {
+    		$weights = array_fill(0, count($sources), 1);
+    	}
+
+    	if (count($sources) !== count($weights)) {
+    		throw new \InvalidArgumentException(sprintf("Mismatch count() array between sources(%s) and weights(%s)", count($sources), count($weights)));
+    	}
+
+   		$fields = $this->getAggregateAttributes($sources, $weights);
+   		$result = $onCreate($fields);
+
+   		if (!$result instanceof Result) {
+    		throw new \InvalidArgumentException(sprintf("Third parameter \$onCreate should return an instance of %s", get_class(Result::class)));
+   		}
+
+   		if ($result->ok()) {
+
+    		foreach ($sources as $key => $source) {
+    			$params = [
+	    			'source_type' => get_class($source),
+	    			'source_id' => $source->id,
+	    			'weight' => $weights[$key],
+	    			'aggregate_type' => get_class($result->getResource()),
+	    			'aggregate_id' => $result->getResource()->id
+	    		];
+	    		$resultAggregatorCreate = $this->createOrFail($params);
+
+	    		$result->addErrors($resultAggregatorCreate->getErrors());
+	    	}
+    	}
+
+    	return $result;
+    }
+
+
+    /**
+     * Aggregate multiple entities to one final entity
+     *
+     * @param array $sources
+     * @param Closure $onRemove
+     *
+     * @return Result
+     */
+    public function removeAggregate(array $sources, Closure $onRemove)
+    {
+    	$sources = Collection::make($sources);
+
+    	$q = $this->getRepository()->newQuery();
+    	
+    	$q->where(function ($q) use ($sources) {
+	    	foreach ($sources as $source) {
+	    		$q->orWhere(function($q) use ($source) {
+	    			$q->where('source_type', get_class($source));
+	    			$q->where('source_id', $source->id);
+	    		});
+	    	}
+	    });
+
+	    $q->get()->map(function($aggregator) {
+	    	$aggregator->aggregate()->delete();
+	    	$aggregator->delete();
+	    });
+
+    	return new Result();
+    }
+    public function getAggregateAttributes(array $sources, array $weights): Collection
+    {
+    	$fields = Collection::make();
+
+    	$fieldsName = array_keys($sources[0]->getAttributes());
+
+    	foreach ($fieldsName as $fieldName) {
+
+    		$score = Collection::make();
+
+    		foreach ($sources as $key => $source) {
+  					
+  				$value = $source->$fieldName;
+  				$weight = (int) $weights[$key];
+
+  				$fieldScore = (new Bag())->set('score', $weight)->set('value', $value);
+
+  				$found = $score->first(function($item) use ($value) {
+
+  					// Strict comparison
+  					return $item->get('value') === $value;
+  				});
+
+  				if ($found) {
+  					$found->set('score', $found->get('score') + $weight);
+  				} else {
+  					$score[] = $fieldScore;
+  				}
+
+    		}
+
+    		$fields[$fieldName] = $score->sortByDesc(function($item) {
+    			return $item->get('score');
+    		})->first()->get('value');
+    	}
+
+    	return $fields;
+
+    }
 }
--- tests/Managers/AggregatorTest.php
+++ tests/Managers/AggregatorTest.php
@@ -6,6 +6,9 @@ use Railken\Amethyst\Fakers\AggregatorFaker;
 use Railken\Amethyst\Managers\AggregatorManager;
 use Railken\Amethyst\Tests\BaseTest;
 use Railken\Lem\Support\Testing\TestableBaseTrait;
+use Railken\Amethyst\Managers\FooManager;
+use Railken\Amethyst\Models\Foo;
+use Railken\Amethyst\Fakers\FooFaker;
 
 class AggregatorTest extends BaseTest
 {
@@ -24,4 +27,32 @@ class AggregatorTest extends BaseTest
      * @var string
      */
     protected $faker = AggregatorFaker::class;
+
+    public function testCreateAggregate()
+    {
+        $fooManager = new FooManager();
+
+        $foo1 = $fooManager->createOrFail(FooFaker::make()->parameters()->set('description', 'A'))->getResource();
+        $foo2 = $fooManager->createOrFail(FooFaker::make()->parameters()->set('description', 'B'))->getResource();
+        $foo3 = $fooManager->createOrFail(FooFaker::make()->parameters()->set('description', 'B'))->getResource();
+        $foo4 = $fooManager->createOrFail(FooFaker::make()->parameters()->set('description', 'C'))->getResource();
+        $foo5 = $fooManager->createOrFail(FooFaker::make()->parameters()->set('description', 'D'))->getResource();
+
+        $aggregatorManager = new AggregatorManager();
+
+        $onCreate = function($fields) use ($fooManager) {
+            return $fooManager->create($fields);
+        };
+
+        $result = $aggregatorManager->createAggregate([$foo1, $foo2, $foo3, $foo4, $foo5], [8, 5, 4, 2, 7], $onCreate);
+
+        $aggregate = $result->getResource();
+
+        $this->assertEquals('B', $aggregate->description);
+
+        $result = $aggregatorManager->removeAggregate([$foo1, $foo2, $foo3, $foo4, $foo5], function() {});
+
+        $this->assertEquals(null, $aggregatorManager->getRepository()->findOneBy(['aggregate_id' => $aggregate->id]));
+        $this->assertEquals(null, $fooManager->getRepository()->findOneBy(['id' => $aggregate->id]));
+    }
 }
