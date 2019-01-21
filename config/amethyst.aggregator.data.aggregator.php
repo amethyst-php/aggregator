8d9216a042b71a82d82cc98d386d3f673b354a51
railken <railken@protonmail.ch>
2019-01-21 16:19:09 +0100

    cleanup

--- config/amethyst.aggregator.data.aggregator.php
+++ config/amethyst.aggregator.data.aggregator.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    'table'       => 'amethyst_aggregators',
-    'comment'     => 'Aggregator',
-    'model'       => Railken\Amethyst\Models\Aggregator::class,
-    'schema'      => Railken\Amethyst\Schemas\AggregatorSchema::class,
-    'repository'  => Railken\Amethyst\Repositories\AggregatorRepository::class,
-    'serializer'  => Railken\Amethyst\Serializers\AggregatorSerializer::class,
-    'validator'   => Railken\Amethyst\Validators\AggregatorValidator::class,
-    'authorizer'  => Railken\Amethyst\Authorizers\AggregatorAuthorizer::class,
-    'faker'       => Railken\Amethyst\Fakers\AggregatorFaker::class,
-    'manager'     => Railken\Amethyst\Managers\AggregatorManager::class,
-    'aggregable'  => [
+    'table'      => 'amethyst_aggregators',
+    'comment'    => 'Aggregator',
+    'model'      => Railken\Amethyst\Models\Aggregator::class,
+    'schema'     => Railken\Amethyst\Schemas\AggregatorSchema::class,
+    'repository' => Railken\Amethyst\Repositories\AggregatorRepository::class,
+    'serializer' => Railken\Amethyst\Serializers\AggregatorSerializer::class,
+    'validator'  => Railken\Amethyst\Validators\AggregatorValidator::class,
+    'authorizer' => Railken\Amethyst\Authorizers\AggregatorAuthorizer::class,
+    'faker'      => Railken\Amethyst\Fakers\AggregatorFaker::class,
+    'manager'    => Railken\Amethyst\Managers\AggregatorManager::class,
+    'aggregable' => [
         Railken\Amethyst\Models\Foo::class => Railken\Amethyst\Managers\FooManager::class,
     ],
 ];
--- config/amethyst.aggregator.http.admin.aggregator.php
+++ config/amethyst.aggregator.http.admin.aggregator.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'enabled'     => true,
-    'controller'  => Railken\Amethyst\Http\Controllers\Admin\AggregatorsController::class,
-    'router'      => [
-        'as'        => 'aggregator.',
-        'prefix'    => '/aggregators',
+    'enabled'    => true,
+    'controller' => Railken\Amethyst\Http\Controllers\Admin\AggregatorsController::class,
+    'router'     => [
+        'as'     => 'aggregator.',
+        'prefix' => '/aggregators',
     ],
 ];
